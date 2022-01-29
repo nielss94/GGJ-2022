8f04e84b7fb24e181a9d23d228d3a47eb01fbdea
Niels van Dam <nielsvdam@hotmail.com>
2022-01-29 21:35:47 +0100

    audio on death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,27 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class Player : MonoBehaviour
 {
     public Transform lampPos;
     public PlayerLamp playerLamp;
-
+    public List<AudioClip> death = new List<AudioClip>();
     public bool dead = false;
+
+    private DarthFader _darthFader;
+
+    private void Awake()
+    {
+        _darthFader = FindObjectOfType<DarthFader>();
+    }
+
     private void Start()
     {
+        if (_darthFader)
+        {
+            _darthFader.FadeIn();
+        }
         Instantiate(playerLamp, lampPos.position, lampPos.rotation);
     }
 
     public void Die()
     {
         dead = true;
+        FindObjectOfType<AudioManager>().PlayOneShot(death[Random.Range(0, death.Count)]);
         StartCoroutine(WaitAndReloadScene());
     }
 
     private IEnumerator WaitAndReloadScene()
     {
-        yield return new WaitForSeconds(1.5f);
+        if (_darthFader)
+        {
+            _darthFader.FadeOut();
+        }
+        yield return new WaitForSeconds(4.5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
