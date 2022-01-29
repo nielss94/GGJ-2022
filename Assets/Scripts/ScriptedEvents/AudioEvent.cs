136910446fb2edba4afa227630e5a0d4b0c44bc9
Niels van Dam <nielsvdam@hotmail.com>
2022-01-29 17:13:21 +0100

    update

--- Assets/Scripts/ScriptedEvents/AudioEvent.cs
+++ Assets/Scripts/ScriptedEvents/AudioEvent.cs
@@ -7,11 +7,18 @@ public class AudioEvent : ScriptedEvent
     public AudioClip audioClip;
     public Transform position;
 
+    public bool destroyOnActivated;
+    
     protected override void Activate()
     {
         if (audioClip)
         {
             FindObjectOfType<AudioManager>().PlayOneShot(audioClip, position ? position.position : Vector3.zero, position ? 1 : 0, 1);
+
+            if (destroyOnActivated)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
