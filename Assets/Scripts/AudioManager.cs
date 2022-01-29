d8e287820fd663a629add11c845677d0fad85048
Niels van Dam <nielsvdam@hotmail.com>
2022-01-29 14:10:02 +0100

    player improvements gp

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,10 +8,11 @@ public class AudioManager : MonoBehaviour
 {
     public AudioClipPlayer audioClipPlayer = null;
 
-    public void PlayOneShot(AudioClip clip, Vector3 pos = default, float spacial = 0)
+    public void PlayOneShot(AudioClip clip, Vector3 pos = default, float spacial = 0, float volume = 1)
     {
         AudioClipPlayer newAudioClipPlayer = Instantiate(audioClipPlayer, pos, Quaternion.identity);
         newAudioClipPlayer.audioSource.spatialBlend = spacial;
+        newAudioClipPlayer.audioSource.volume = volume;
         newAudioClipPlayer.PlayOneShot(clip);
     }
 }
