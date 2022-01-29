5a23c161fe47b1766e775c5876ffb1d026880e54
Niels van Dam <nielsvdam@hotmail.com>
2022-01-30 00:52:40 +0100

    no crouch, gem particles removed

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Gem : MonoBehaviour, IPickUp
 {
     public AudioClip pickUpSound;
-
+    public ParticleSystem particleSystem;
     public void PickUp()
     {
         FindObjectOfType<AudioManager>().PlayOneShot(pickUpSound, transform.position);
+        StartCoroutine(RemoveParticles());
+    }
+
+    private IEnumerator RemoveParticles()
+    {
+        var emission = particleSystem.emission; // Stores the module in a local variable
+        emission.enabled = false; 
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
     }
 }
