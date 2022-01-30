d6408c8456999377f9538fbd82ab29bbf818f454
Chris <christian-boer@hotmail.com>
2022-01-30 15:10:10 +0100

    Catimations

--- Assets/Scripts/CatAnimator.cs
+++ Assets/Scripts/CatAnimator.cs
@@ -2,27 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatAnimator : MonoBehaviour {
     [SerializeField] private float stationaryThresholdSpeed = 0.3f;
     
     private Animator animator;
-    private Rigidbody rigidbody;
+    private NavMeshAgent navAgent;
+    private CatAggro catAggro;
+    private float velocity;
 
     private void Awake() {
         animator = GetComponentInChildren<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
+        catAggro = GetComponent<CatAggro>();
+        navAgent = GetComponent<NavMeshAgent>();
+
+        catAggro.onChaseStarted += ActivateChase;
+        catAggro.onChaseEnded += DeactivateChase;
     }
 
     private void Update() {
-        var velMag = rigidbody.velocity.magnitude;
+        velocity = navAgent.velocity.magnitude;
 
-        if (velMag > stationaryThresholdSpeed) {
+        if (velocity > stationaryThresholdSpeed) {
             animator.SetBool("moving", true);
         } else {
             animator.SetBool("moving", false);
         }
         
-        animator.SetFloat("velocity", rigidbody.velocity.magnitude);
+        animator.SetFloat("velocity", navAgent.velocity.magnitude);
+    }
+
+    private void ActivateChase() {
+        animator.SetBool("chasing", true);
+    }
+
+    private void DeactivateChase() {
+        animator.SetBool("chasing" , false);
+    }
+
+    private void OnDestroy() {
+        catAggro.onChaseStarted -= ActivateChase;
+        catAggro.onChaseEnded -= DeactivateChase;
     }
 }
