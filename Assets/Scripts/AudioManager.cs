9fbd31a43cce08af535ab7420469b85bbc709676
Niels van Dam <nielsvdam@hotmail.com>
2022-01-30 01:43:37 +0100

    change audio by gem amount

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,6 +8,10 @@ public class AudioManager : MonoBehaviour
 {
     public AudioClipPlayer audioClipPlayer = null;
 
+    public AudioSource environmentAudio;
+    public AudioClip firstDrone;
+    public AudioClip secondDrone;
+    public AudioClip thirdDrone;
     public void PlayOneShot(AudioClip clip, Vector3 pos = default, float spacial = 0, float volume = 1, Transform parent = null)
     {
         AudioClipPlayer newAudioClipPlayer = Instantiate(audioClipPlayer, pos, Quaternion.identity);
@@ -15,5 +19,21 @@ public class AudioManager : MonoBehaviour
         newAudioClipPlayer.audioSource.volume = volume;
         newAudioClipPlayer.transform.SetParent(parent);
         newAudioClipPlayer.PlayOneShot(clip);
+    }
+
+    public void CheckForMusicChange(int totalGems)
+    {
+        if (totalGems >= 5 && environmentAudio.clip != thirdDrone)
+        {
+            environmentAudio.clip = thirdDrone;
+        }
+        else if (totalGems >= 2 && environmentAudio.clip != secondDrone)
+        {
+            environmentAudio.clip = secondDrone;
+        }
+        else if (environmentAudio.clip != firstDrone)
+        {
+            environmentAudio.clip = firstDrone;
+        }
     }
 }
