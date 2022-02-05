4d87d3916cc768109d8075ff1cf44fcbfc575b98
Eisoniq <eisoniq@gmail.com>
2022-02-05 16:09:58 +0100

    Laatste updates
    Laatste updates

---
+++ Assets/Scripts/UI/PressToQuit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressToQuit : MonoBehaviour
+{
+	public void OnQuit() {
+		Application.Quit();
+	}
+}
